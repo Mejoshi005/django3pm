d7b6875fd02c39967ea0e4293b1986fb3790a7ac
Mejoshi005 <sushantjoshi005@gmail.com>
2023-01-24 16:15:28 +0545

    templates added

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
+
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
+
 def contact(request):
     return render(request, 'contact.html')
 
